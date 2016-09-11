0f187d04dd2c2e3f249d1b2498a24456c8191b27
Orion10110 <pojasoriona@gmail.com>
2016-09-11 17:13:21 +0300

    Added changed DB

--- EnergyConsumption/Controllers/HomeController.cs
+++ EnergyConsumption/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EnergyConsumption.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +11,33 @@ namespace EnergyConsumption.Controllers
 {
     public class HomeController : Controller
     {
+
+
+         private UserManager<ApplicationUser> manager;
+        private ApplicationDbContext db;
+
+        public HomeController()
+        {
+            db = new ApplicationDbContext();
+            manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
+
+        // GET: ToDoes
         public ActionResult Index()
         {
-            return View();
+            var currentUser = manager.FindById(User.Identity.GetUserId());
+            var todo = db.Homes.Where(t => t.UserId == currentUser.Id).ToList();
+            return View(todo);
         }
+      
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            if(User.Identity.IsAuthenticated)
+            {
+                
+            }
             return View();
         }
 
