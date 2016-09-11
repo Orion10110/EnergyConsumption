136706cc2eafbeb1fefade221277f8f21a79347c
Orion10110 <pojasoriona@gmail.com>
2016-09-11 13:01:50 +0300

    creating DB

---
+++ EnergyConsumption/Models/Device.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnergyConsumption.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int W { get; set; }
+        public double Hour { get; set; }
+        public double Day { get; set; }
+
+        public int? IdHome { get; set; }
+        public Home Home { get; set; }
+
+    }
+
+    public class Home
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Device> Devices { get; set; }
+
+        public Home()
+        {
+            Devices = new List<Device>();
+        }
+
+        public string UserId { get; set; }
+    }
+
+}
