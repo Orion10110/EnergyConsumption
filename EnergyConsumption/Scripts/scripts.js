5f94138e3be8713b0b74dc885e6d91aae6af5173
Orion10110 <pojasoriona@gmail.com>
2016-09-19 22:28:20 +0300

    Vn added script

---
+++ EnergyConsumption/Scripts/scripts.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("a.Edit").click(function (e) {
+        e.preventDefault();
+        alert("as");
+        a = $(this).parent().parent().children();
+        for (var i = 0; i < a.length - 1; i++) {
+            alert(($(a[i])).text());
+        }
+        
+    })
+});
